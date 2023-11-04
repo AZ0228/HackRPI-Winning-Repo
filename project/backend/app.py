b25fc6c4c223e823fc85e00f30e7d2a36ee0a82f
AZ0228 <53315675+AZ0228@users.noreply.github.com>
2023-11-04 16:15:54 -0400

    added chat routes

--- project/backend/app.py
+++ project/backend/app.py
@@ -1,5 +1,7 @@
 from flask import Flask, jsonify, request
 from data import parse_csv
+from chat import get_response
+import datetime
 
 app = Flask(__name__)
 
@@ -7,6 +9,17 @@ app = Flask(__name__)
 def hello(query, value):
     result = parse_csv('emissions.csv', {query: value})
     return jsonify(result), 200
+
+
+@app.route('/info/<name>', methods=['GET'])
+def info(name):
+    now = datetime.datetime.now()
+    ret = {
+        'result' : get_response(name),
+    }
+    elapsed = f"{round((datetime.datetime.now() - now).total_seconds())} seconds"
+    ret['time'] = elapsed
+    return jsonify(ret), 200
 
 @app.route('/echo', methods=['POST'])
 def echo():
