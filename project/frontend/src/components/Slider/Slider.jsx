b9b7bcce87186a90f0e44e4f20817b2c8e408a6a
milesthomas <ths22115@towerhill.org>
2023-11-05 08:39:04 -0500

    slider current year done, styled home page as well

--- project/frontend/src/components/Slider/Slider.jsx
+++ project/frontend/src/components/Slider/Slider.jsx
@@ -2,15 +2,26 @@ import * as React from 'react';
 import Box from '@mui/material/Box';
 // import Slider from '@mui/material/Slider';
 import Slider from '@mui/joy/Slider';
-
 import './Styles.css';
+import {useEffect, useState} from "react";
+import data from "../../assets/data.geojson";
+import { useFectchYear } from '../../hooks/useFetchYear.js';
 
-export default function VerticalSlider() {
-  function preventHorizontalKeyboardNavigation(event) {
-    if (event.key === 'ArrowLeft' || event.key === 'ArrowRight') {
-      event.preventDefault();
+const VerticalSlider = () => {
+  const [currentYear, setCurrentYear] = useState(valToYear(226));
+
+  function valToYear(currentValue) {
+    if (0 <= currentValue && currentValue <= 5) {
+      return (1750 + 10*currentValue);
+    } else {
+      return (1800 + (currentValue - 5));
     }
   }
+
+  useEffect(() => {
+    console.log(currentYear);
+  });
+
 
   return (
       <div style={{height: '60%', width:"fit-content"}}>
@@ -23,9 +34,14 @@ export default function VerticalSlider() {
             max={226}
             min={0}
             defaultValue={226}
+            onChange={(event, value) => {
+              setCurrentYear(valToYear(value));
+            }
+            }
 
         />
       </div>
 
   );
 }
+export default VerticalSlider;
