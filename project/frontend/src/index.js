04a02f139b4ff6e8c65097cb7ccbe0b1ecde7456
milesthomas <ths22115@towerhill.org>
2023-11-05 00:18:59 -0400

    globe!

--- project/frontend/src/index.js
+++ project/frontend/src/index.js
@@ -4,11 +4,12 @@ import './index.css';
 import App from './App';
 import Test from './Testing';
 import reportWebVitals from './reportWebVitals';
+import GlobeTest from "./pages/GlobeTest/GlobeTest";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <GlobeTest />
     <Test query="Entity" value="United States" />
   </React.StrictMode>
 );
